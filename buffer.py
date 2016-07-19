99a847eade134b97b445a1c05cc919b6f9b1fe93
Eric Cox <emcox84@gmail.com>
2016-07-19 06:13:36 -0400

    adding full trace to single machine version

--- buffer.py
+++ buffer.py
@@ -5,6 +5,7 @@ Author:
 Eric Cox
 """
 import tensorflow as tf
+from tensorflow.core.protobuf import config_pb2
 import time
 from datetime import datetime
 
@@ -14,7 +15,7 @@ tf.app.flags.DEFINE_integer('variable_size', 1024, """Variable size.""")
 tf.app.flags.DEFINE_integer('batch_size', 10, """benchmark iteration size.""")
 tf.app.flags.DEFINE_string('node_name', None, """Node name (job)""")
 tf.app.flags.DEFINE_integer('task_index', 0, """task index""")
-tf.app.flags.DEFINE_bool('enable_trace', False, 'Enable trace')
+tf.app.flags.DEFINE_bool('full_trace', 0, 'Enable trace')
 
 def get_run_op():
     variable_size = int(FLAGS.variable_size)
@@ -28,16 +29,30 @@ def time_tensorflow_run(sess, init_op, buf_op):
     with sess:
         sess.run(init_op)
         for i in range(FLAGS.batch_size + num_steps_burn_in):
+            if FLAGS.full_trace == 1 and i == num_steps_burn_in - 1:
+                run_options = config_pb2.RunOptions(
+                    trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = config_pb2.RunMetadata()
+            else:
+                run_options = None
+                run_metadata = None
+
             start_time = time.time()
-            result = sess.run(buf_op)
+            _ = sess.run(buf_op, options=run_options, run_metadata=run_metadata)
             duration = time.time() - start_time
+            if FLAGS.full_trace == 1 and i == num_steps_burn_in - 1:
+                tl = tf.python.client.timeline.Timeline(run_metadata.step_stats)
+                ctf = tl.generate_chrome_trace_format()
+                with open('full_trace.ctf', 'w') as f:
+                    f.write(ctf)
+
             if i >= num_steps_burn_in:
                 if not i % 10:
                     print ('%s: step %d, duration = %.3f' %
                         (datetime.now(), i - num_steps_burn_in, duration))
-            total_duration += duration
+                total_duration += duration
 
-    avg_total_duration = total_duration / (FLAGS.batch_size + num_steps_burn_in)
+    avg_total_duration = total_duration / (FLAGS.batch_size)
     print ("total_duration avg = %.6f" % avg_total_duration)
 
 def main(_):
